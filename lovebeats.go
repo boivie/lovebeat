cf01d1e9b5671ac26a8fec8ee0b27109de5a2fb8
Victor Boivie <victor@boivie.com>
2014-04-05 09:21:18 +0200

    Initial view support

--- lovebeats.go
+++ lovebeats.go
@@ -53,6 +53,21 @@ type Cmd struct {
 	Value    int
 }
 
+const (
+	ACTION_REFRESH_VIEW = "refresh-view"
+)
+
+type ViewCmd struct {
+	Action   string
+	View     string
+}
+
+type View struct {
+	Name           string
+	State          string
+	LastUpdated    int64
+}
+
 type Service struct {
 	Name           string
 	LastValue      int
@@ -64,7 +79,8 @@ type Service struct {
 }
 
 var (
-	In       = make(chan *Cmd, MAX_UNPROCESSED_PACKETS)
+	ServiceCmdChan    = make(chan *Cmd, MAX_UNPROCESSED_PACKETS)
+	ViewCmdChan         = make(chan *ViewCmd, MAX_UNPROCESSED_PACKETS)
 	signalchan chan os.Signal
 	client redis.Client
 )
@@ -141,17 +157,17 @@ func updateExpiry(service *Service, ts int64) {
 func (s *Service)Save(ref *Service, ts int64) {
 	if *s != *ref {
 		if s.State != ref.State {
-			log.Printf("service %s, state %s -> %s",
+			log.Printf("SERVICE '%s', state %s -> %s",
 				s.Name, ref.State, s.State)
 			s.Log("%d|state|%s", ts, s.State)
 		}
 		if s.WarningTimeout != ref.WarningTimeout {
-			log.Printf("service %s, warn %d -> %d",
+			log.Printf("SERVICE '%s', warn %d -> %d",
 				s.Name, ref.WarningTimeout, s.WarningTimeout)
 			s.Log("%d|warn-tmo|%s", ts, ref.WarningTimeout)
 		}
 		if s.ErrorTimeout != ref.ErrorTimeout {
-			log.Printf("service %s, err %d -> %d",
+			log.Printf("SERVICE '%s', err %d -> %d",
 				s.Name, ref.ErrorTimeout, s.ErrorTimeout)
 			s.Log("%d|err-tmo|%s", ts, ref.ErrorTimeout)
 		}
@@ -163,6 +179,70 @@ func (s *Service)Save(ref *Service, ts int64) {
 		}
 	}
 }
+
+func getView(name string) (*View, *View) {
+	view := &View{
+		Name: name,
+		State: STATE_OK,
+	}
+
+	if data, err := client.Get("lb.view." + name); err == nil {
+		json.Unmarshal(data, &view)
+	}
+	var ref = *view
+	return view, &ref
+}
+
+func (v *View) Refresh(ts int64) {
+	var services, _ = client.Smembers("lb.view-contents." + v.Name)
+	v.State = STATE_OK
+
+	for _, serv := range services {
+		var service, _ = getOrCreate(string(serv))
+		if service.State == STATE_WARNING && v.State == STATE_OK  {
+			v.State = STATE_WARNING
+		} else if service.State == STATE_ERROR {
+			v.State = STATE_ERROR
+		}
+	}
+}
+
+func (v *View) Log(format string, args ...interface{}) {
+	var key = "lb.view-log." + v.Name
+	var log = fmt.Sprintf(format, args...)
+	client.Lpush(key, []byte(log))
+	client.Ltrim(key, 0, MAX_LOG_ENTRIES)
+}
+
+func (v *View) Save(ref *View, ts int64) {
+	if *v != *ref {
+		if v.State != ref.State {
+			log.Printf("VIEW '%s', state %s -> %s",
+				v.Name, ref.State, v.State)
+			v.Log("%d|state|%s", ts, v.State)
+		}
+		v.LastUpdated = ts
+		b, _ := json.Marshal(v)
+		client.Set("lb.view." + v.Name, b)
+	}
+}
+
+func (s *Service) triggerViews() {
+	var views, _ = client.Smembers("lb.views.all")
+
+	for _, view := range views {
+		var view_name = string(view)
+		var mbr, _ = client.Sismember("lb.view-contents." + view_name,
+			[]byte(s.Name));
+		if mbr {
+			ViewCmdChan <- &ViewCmd{
+				Action: ACTION_REFRESH_VIEW,
+				View:   view_name,
+			}
+		}
+	}
+}
+
 
 func monitor() {
 	period := time.Duration(*expiryInterval) * time.Second
@@ -180,9 +260,18 @@ func monitor() {
 					service.UpdateState(ts)
 					service.Save(ref, ts)
 					updateExpiry(service, ts)
+					service.triggerViews()
 				}
 			}
-		case s := <-In:
+		case s := <-ViewCmdChan:
+			var ts = now()
+			switch s.Action {
+			case ACTION_REFRESH_VIEW:
+				var view, ref = getView(s.View)
+				view.Refresh(ts)
+				view.Save(ref, ts);
+			}
+		case s := <-ServiceCmdChan:
 			var ts = now()
 			var service, ref = getOrCreate(s.Service)
 			switch s.Action {
@@ -198,6 +287,7 @@ func monitor() {
 			service.UpdateState(ts)
 			service.Save(ref, ts)
 			updateExpiry(service, ts)
+			service.triggerViews()
 		}
 	}
 }
@@ -273,7 +363,7 @@ func udpListener() {
 		}
 
 		for _, p := range parseMessage(message[:n]) {
-			In <- p
+			ServiceCmdChan <- p
 		}
 	}
 }
@@ -340,7 +430,7 @@ func TriggerHandler(c http.ResponseWriter, r *http.Request) {
 
 	var errtmo, warntmo = r.FormValue("err-tmo"), r.FormValue("warn-tmo")
 
-	In <- &Cmd{
+	ServiceCmdChan <- &Cmd{
 		Action:  ACTION_BEAT,
 		Service: name,
 		Value:   1,
@@ -348,7 +438,7 @@ func TriggerHandler(c http.ResponseWriter, r *http.Request) {
 
 	
 	if val, err := strconv.Atoi(errtmo); err == nil {
-		In <- &Cmd{
+		ServiceCmdChan <- &Cmd{
 			Action:  ACTION_SET_ERR,
 			Service: name,
 			Value:   val,
@@ -356,7 +446,7 @@ func TriggerHandler(c http.ResponseWriter, r *http.Request) {
 	}
 
 	if val, err := strconv.Atoi(warntmo); err == nil {
-		In <- &Cmd{
+		ServiceCmdChan <- &Cmd{
 			Action:  ACTION_SET_WARN,
 			Service: name,
 			Value:   val,
@@ -369,11 +459,45 @@ func TriggerHandler(c http.ResponseWriter, r *http.Request) {
         io.WriteString(c, "ok\n")
 }
 
+func CreateViewHandler(c http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	view_name := params["name"]
+	var expr = r.FormValue("regexp")
+	if expr == "" {
+		log.Printf("No regexp provided")
+		return
+	}
+
+	var re, err = regexp.Compile(expr)
+	if err != nil {
+		log.Printf("Invalid regexp: %s", err)
+		return
+	}
+
+	// Match services
+	var key = "lb.view-contents." + view_name
+	client.Del(key)
+	var services, _ = client.Smembers("lb.services.all")
+	for _, name := range services {
+		if re.Match(name) {
+			client.Sadd(key, name)
+		}
+	}
+
+	client.Sadd("lb.views.all", []byte(view_name))
+	log.Printf("VIEW '%s' created or updated.", view_name)
+	ViewCmdChan <- &ViewCmd{
+		Action: ACTION_REFRESH_VIEW,
+		View:   view_name,
+	}
+}
+
 func httpServer() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", DashboardHandler).Methods("GET")
 	rtr.HandleFunc("/status", StatusHandler).Methods("GET")
-	rtr.HandleFunc("/trigger/{name:[a-z.]+}", TriggerHandler).Methods("POST")
+	rtr.HandleFunc("/trigger/{name:[a-z0-9.]+}", TriggerHandler).Methods("POST")
+	rtr.HandleFunc("/view/{name:[a-z0-9.]+}", CreateViewHandler).Methods("POST")
 	http.Handle("/", rtr)
         http.ListenAndServe(":8080", nil)
 }
