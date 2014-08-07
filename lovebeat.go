1d66e108cab089f078f20bf4dd43e41cccd916df
Victor Boivie <victor@boivie.com>
2014-08-07 18:29:45 +0200

    Refactoring

--- lovebeat.go
+++ lovebeat.go
@@ -6,7 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"net"
-	"encoding/json"
 	"os"
 	"os/signal"
 	"regexp"
@@ -16,10 +15,10 @@ import (
 	"time"
 	"io"
 	"net/http"
-	"github.com/hoisie/redis"
 	"github.com/gorilla/mux"
 	"github.com/op/go-logging"
 	"html/template"
+	"github.com/boivie/lovebeat-go/service"
 )
 
 var log = logging.MustGetLogger("lovebeat")
@@ -27,7 +26,6 @@ var log = logging.MustGetLogger("lovebeat")
 const (
 	VERSION                 = "0.1.0"
 	MAX_UNPROCESSED_PACKETS = 1000
-	MAX_LOG_ENTRIES         = 1000
 	MAX_UDP_PACKET_SIZE     = 512
 )
 
@@ -44,208 +42,19 @@ const (
 	ACTION_BEAT = "beat"
 )
 
-const (
-	STATE_PAUSED  = "paused"
-	STATE_OK      = "ok"
-	STATE_WARNING = "warning"
-	STATE_ERROR   = "error"
-)
-
 type Cmd struct {
 	Action   string
 	Service  string
 	Value    int
 }
 
-const (
-	ACTION_REFRESH_VIEW = "refresh-view"
-)
-
-type ViewCmd struct {
-	Action   string
-	View     string
-}
-
-type View struct {
-	Name           string
-	State          string
-	LastUpdated    int64
-}
-
-type Service struct {
-	Name           string
-	LastValue      int
-	LastBeat       int64
-	LastUpdated    int64
-	WarningTimeout int64
-	ErrorTimeout   int64
-	State          string
-}
-
 var (
 	ServiceCmdChan    = make(chan *Cmd, MAX_UNPROCESSED_PACKETS)
-	ViewCmdChan         = make(chan *ViewCmd, MAX_UNPROCESSED_PACKETS)
+	ViewCmdChan         = make(chan *service.ViewCmd, MAX_UNPROCESSED_PACKETS)
 	signalchan chan os.Signal
-	client redis.Client
 )
 
 func now() int64 { return time.Now().Unix() }
-
-func getOrCreate(name string) (*Service, *Service) {
-	service := &Service{
-		Name: name,
-		LastValue: -1,
-		LastBeat: -1,
-		LastUpdated: -1,
-		WarningTimeout: -1,
-		ErrorTimeout: -1,
-		State: STATE_PAUSED,
-	}
-
-	if data, err := client.Get("lb.service." + name); err == nil {
-		json.Unmarshal(data, &service)
-	}
-	var ref = *service
-	return service, &ref
-}
-
-func (s *Service)GetExpiry(timeout int64) int64 {
-	if timeout <= 0 {
-		return 0
-	}
-	return s.LastBeat + timeout
-}
-
-func (s *Service)GetNextExpiry(ts int64) int64 {
-	var next int64 = 0
-	var warningExpiry = s.GetExpiry(s.WarningTimeout)
-	var errorExpiry = s.GetExpiry(s.ErrorTimeout)
-	if warningExpiry > 0 && warningExpiry > ts && (next == 0 || warningExpiry < next) {
-		next = warningExpiry
-	}
-	if errorExpiry > 0 && errorExpiry > ts && (next == 0 || errorExpiry < next) {
-		next = errorExpiry
-	}
-	return next
-}
-
-func (s *Service) UpdateState(ts int64) {
-	s.State = STATE_OK
-	var warningExpiry = s.GetExpiry(s.WarningTimeout)
-	var errorExpiry = s.GetExpiry(s.ErrorTimeout)
-	if warningExpiry > 0 && ts >= warningExpiry {
-		s.State = STATE_WARNING
-	}
-	if errorExpiry > 0 && ts >= errorExpiry {
-		s.State = STATE_ERROR
-	}
-}
-
-func (s *Service) Log(format string, args ...interface{}) {
-	var key = "lb.service-log." + s.Name
-	var log = fmt.Sprintf(format, args...)
-	client.Lpush(key, []byte(log))
-	client.Ltrim(key, 0, MAX_LOG_ENTRIES)
-}
-
-func updateExpiry(service *Service, ts int64) {
-	if service.State != STATE_PAUSED {
-		if expiry := service.GetNextExpiry(ts); expiry > 0 {
-			client.Zadd("lb.expiry", []byte(service.Name), float64(expiry))
-			return
-		}
-	}
-	client.Zrem("lb.expiry", []byte(service.Name))
-}
-
-func (s *Service)Save(ref *Service, ts int64) {
-	if *s != *ref {
-		if s.State != ref.State {
-			log.Info("SERVICE '%s', state %s -> %s",
-				s.Name, ref.State, s.State)
-			s.Log("%d|state|%s", ts, s.State)
-		}
-		if s.WarningTimeout != ref.WarningTimeout {
-			log.Info("SERVICE '%s', warn %d -> %d",
-				s.Name, ref.WarningTimeout, s.WarningTimeout)
-			s.Log("%d|warn-tmo|%s", ts, ref.WarningTimeout)
-		}
-		if s.ErrorTimeout != ref.ErrorTimeout {
-			log.Info("SERVICE '%s', err %d -> %d",
-				s.Name, ref.ErrorTimeout, s.ErrorTimeout)
-			s.Log("%d|err-tmo|%s", ts, ref.ErrorTimeout)
-		}
-		s.LastUpdated = ts
-		b, _ := json.Marshal(s)
-		client.Set("lb.service." + s.Name, b)
-		if ref.LastUpdated < 0 {
-			client.Sadd("lb.services.all", []byte(s.Name))
-		}
-	}
-}
-
-func getView(name string) (*View, *View) {
-	view := &View{
-		Name: name,
-		State: STATE_OK,
-	}
-
-	if data, err := client.Get("lb.view." + name); err == nil {
-		json.Unmarshal(data, &view)
-	}
-	var ref = *view
-	return view, &ref
-}
-
-func (v *View) Refresh(ts int64) {
-	var services, _ = client.Smembers("lb.view-contents." + v.Name)
-	v.State = STATE_OK
-
-	for _, serv := range services {
-		var service, _ = getOrCreate(string(serv))
-		if service.State == STATE_WARNING && v.State == STATE_OK  {
-			v.State = STATE_WARNING
-		} else if service.State == STATE_ERROR {
-			v.State = STATE_ERROR
-		}
-	}
-}
-
-func (v *View) Log(format string, args ...interface{}) {
-	var key = "lb.view-log." + v.Name
-	var log = fmt.Sprintf(format, args...)
-	client.Lpush(key, []byte(log))
-	client.Ltrim(key, 0, MAX_LOG_ENTRIES)
-}
-
-func (v *View) Save(ref *View, ts int64) {
-	if *v != *ref {
-		if v.State != ref.State {
-			log.Info("VIEW '%s', state %s -> %s",
-				v.Name, ref.State, v.State)
-			v.Log("%d|state|%s", ts, v.State)
-		}
-		v.LastUpdated = ts
-		b, _ := json.Marshal(v)
-		client.Set("lb.view." + v.Name, b)
-	}
-}
-
-func (s *Service) triggerViews() {
-	var views, _ = client.Smembers("lb.views.all")
-
-	for _, view := range views {
-		var view_name = string(view)
-		var mbr, _ = client.Sismember("lb.view-contents." + view_name,
-			[]byte(s.Name));
-		if mbr {
-			ViewCmdChan <- &ViewCmd{
-				Action: ACTION_REFRESH_VIEW,
-				View:   view_name,
-			}
-		}
-	}
-}
 
 
 func monitor() {
@@ -258,26 +67,24 @@ func monitor() {
 			return
 		case <-ticker.C:
 			var ts = now()
-			if expired, err := client.Zrangebyscore("lb.expiry", 0, float64(now())); err == nil {
-				for _, elem := range expired {
-					var service, ref = getOrCreate(string(elem))
-					service.UpdateState(ts)
-					service.Save(ref, ts)
-					updateExpiry(service, ts)
-					service.triggerViews()
-				}
+			for _, s := range service.GetExpired(ts) {
+				var ref = *s
+				s.UpdateState(ts)
+				s.Save(&ref, ts)
+				s.UpdateExpiry(ts)
+				s.UpdateViews(ViewCmdChan)
 			}
 		case s := <-ViewCmdChan:
 			var ts = now()
 			switch s.Action {
-			case ACTION_REFRESH_VIEW:
-				var view, ref = getView(s.View)
+			case service.ACTION_REFRESH_VIEW:
+				var view, ref = service.GetView(s.View)
 				view.Refresh(ts)
 				view.Save(ref, ts);
 			}
 		case s := <-ServiceCmdChan:
 			var ts = now()
-			var service, ref = getOrCreate(s.Service)
+			var service, ref = service.GetOrCreate(s.Service)
 			switch s.Action {
 			case ACTION_SET_WARN:
 				service.WarningTimeout = int64(s.Value)
@@ -294,8 +101,8 @@ func monitor() {
 			}
 			service.UpdateState(ts)
 			service.Save(ref, ts)
-			updateExpiry(service, ts)
-			service.triggerViews()
+			service.UpdateExpiry(ts)
+			service.UpdateViews(ViewCmdChan)
 		}
 	}
 }
@@ -407,20 +214,14 @@ func tcpListener() {
 
 func DashboardState(in string) string {
 	return map[string]string {
-		STATE_WARNING: "  WARN   ",
-		STATE_ERROR:   "      ERR",
-		STATE_OK:      "OK       ",
+		service.STATE_WARNING: "  WARN   ",
+		service.STATE_ERROR:   "      ERR",
+		service.STATE_OK:      "OK       ",
 	}[in]
 }
 
 func DashboardHandler(w http.ResponseWriter, r *http.Request) {
-	var names, _ = client.Smembers("lb.services.all")
-	services := make(map[string]*Service)
-
-	for _, name := range names {
-		var service, _ = getOrCreate(string(name))
-		services[string(name)] = service
-	}
+	var services = service.GetServices()
 
 	tc := make(map[string]interface{})
 	tc["services"] = services
@@ -433,13 +234,12 @@ func DashboardHandler(w http.ResponseWriter, r *http.Request) {
 
 func StatusHandler(c http.ResponseWriter, req *http.Request) {
 	var buffer bytes.Buffer
-	var services, _ = client.Smembers("lb.services.all")
+	var services = service.GetServices()
 	var errors, warnings, ok = 0, 0, 0
-	for _, v := range services {
-		var service, _ = getOrCreate(string(v))
-		if service.State == STATE_WARNING {
+	for _, s := range services {
+		if s.State == service.STATE_WARNING {
 			warnings++
-		} else if service.State == STATE_ERROR {
+		} else if s.State == service.STATE_ERROR {
 			errors++
 		} else {
 			ok++
@@ -511,22 +311,7 @@ func CreateViewHandler(c http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Match services
-	var key = "lb.view-contents." + view_name
-	client.Del(key)
-	var services, _ = client.Smembers("lb.services.all")
-	for _, name := range services {
-		if re.Match(name) {
-			client.Sadd(key, name)
-		}
-	}
-
-	client.Sadd("lb.views.all", []byte(view_name))
-	log.Info("VIEW '%s' created or updated.", view_name)
-	ViewCmdChan <- &ViewCmd{
-		Action: ACTION_REFRESH_VIEW,
-		View:   view_name,
-	}
+	service.CreateView(view_name, re, ViewCmdChan)
 }
 
 func httpServer(port int16) {
---
+++ service/service.go
@@ -0,0 +1,267 @@
+package service
+
+import (
+	"fmt"
+	"regexp"
+	"encoding/json"
+	"github.com/hoisie/redis"
+	"github.com/op/go-logging"
+)
+
+const (
+	STATE_PAUSED  = "paused"
+	STATE_OK      = "ok"
+	STATE_WARNING = "warning"
+	STATE_ERROR   = "error"
+)
+
+const (
+	MAX_LOG_ENTRIES         = 1000
+)
+
+const (
+	ACTION_REFRESH_VIEW = "refresh-view"
+)
+
+
+
+var (
+	client redis.Client
+)
+
+var log = logging.MustGetLogger("lovebeat")
+
+type Service struct {
+	Name           string
+	LastValue      int
+	LastBeat       int64
+	LastUpdated    int64
+	WarningTimeout int64
+	ErrorTimeout   int64
+	State          string
+}
+
+type View struct {
+	Name           string
+	State          string
+	LastUpdated    int64
+}
+
+
+type ViewCmd struct {
+	Action   string
+	View     string
+}
+
+func GetOrCreate(name string) (*Service, *Service) {
+	service := &Service{
+		Name: name,
+		LastValue: -1,
+		LastBeat: -1,
+		LastUpdated: -1,
+		WarningTimeout: -1,
+		ErrorTimeout: -1,
+		State: STATE_PAUSED,
+	}
+
+	if data, err := client.Get("lb.service." + name); err == nil {
+		json.Unmarshal(data, &service)
+	}
+	var ref = *service
+	return service, &ref
+}
+
+func (s *Service)GetExpiry(timeout int64) int64 {
+	if timeout <= 0 {
+		return 0
+	}
+	return s.LastBeat + timeout
+}
+
+func (s *Service)GetNextExpiry(ts int64) int64 {
+	var next int64 = 0
+	var warningExpiry = s.GetExpiry(s.WarningTimeout)
+	var errorExpiry = s.GetExpiry(s.ErrorTimeout)
+	if warningExpiry > 0 && warningExpiry > ts && (next == 0 || warningExpiry < next) {
+		next = warningExpiry
+	}
+	if errorExpiry > 0 && errorExpiry > ts && (next == 0 || errorExpiry < next) {
+		next = errorExpiry
+	}
+	return next
+}
+
+func (s *Service) UpdateState(ts int64) {
+	s.State = STATE_OK
+	var warningExpiry = s.GetExpiry(s.WarningTimeout)
+	var errorExpiry = s.GetExpiry(s.ErrorTimeout)
+	if warningExpiry > 0 && ts >= warningExpiry {
+		s.State = STATE_WARNING
+	}
+	if errorExpiry > 0 && ts >= errorExpiry {
+		s.State = STATE_ERROR
+	}
+}
+
+func (s *Service) Log(format string, args ...interface{}) {
+	var key = "lb.service-log." + s.Name
+	var log = fmt.Sprintf(format, args...)
+	client.Lpush(key, []byte(log))
+	client.Ltrim(key, 0, MAX_LOG_ENTRIES)
+}
+
+func (s *Service)Save(ref *Service, ts int64) {
+	if *s != *ref {
+		if s.State != ref.State {
+			log.Info("SERVICE '%s', state %s -> %s",
+				s.Name, ref.State, s.State)
+			s.Log("%d|state|%s", ts, s.State)
+		}
+		if s.WarningTimeout != ref.WarningTimeout {
+			log.Info("SERVICE '%s', warn %d -> %d",
+				s.Name, ref.WarningTimeout, s.WarningTimeout)
+			s.Log("%d|warn-tmo|%s", ts, ref.WarningTimeout)
+		}
+		if s.ErrorTimeout != ref.ErrorTimeout {
+			log.Info("SERVICE '%s', err %d -> %d",
+				s.Name, ref.ErrorTimeout, s.ErrorTimeout)
+			s.Log("%d|err-tmo|%s", ts, ref.ErrorTimeout)
+		}
+		s.LastUpdated = ts
+		b, _ := json.Marshal(s)
+		client.Set("lb.service." + s.Name, b)
+		if ref.LastUpdated < 0 {
+			client.Sadd("lb.services.all", []byte(s.Name))
+		}
+	}
+}
+
+func (v *View) Refresh(ts int64) {
+	var services, _ = client.Smembers("lb.view-contents." + v.Name)
+	v.State = STATE_OK
+
+	for _, serv := range services {
+		var service, _ = GetOrCreate(string(serv))
+		if service.State == STATE_WARNING && v.State == STATE_OK  {
+			v.State = STATE_WARNING
+		} else if service.State == STATE_ERROR {
+			v.State = STATE_ERROR
+		}
+	}
+}
+
+func (v *View) Log(format string, args ...interface{}) {
+	var key = "lb.view-log." + v.Name
+	var log = fmt.Sprintf(format, args...)
+	client.Lpush(key, []byte(log))
+	client.Ltrim(key, 0, MAX_LOG_ENTRIES)
+}
+
+func (v *View) Save(ref *View, ts int64) {
+	if *v != *ref {
+		if v.State != ref.State {
+			log.Info("VIEW '%s', state %s -> %s",
+				v.Name, ref.State, v.State)
+			v.Log("%d|state|%s", ts, v.State)
+		}
+		v.LastUpdated = ts
+		b, _ := json.Marshal(v)
+		client.Set("lb.view." + v.Name, b)
+	}
+}
+
+func (s *Service) UpdateExpiry(ts int64) {
+	if s.State != STATE_PAUSED {
+		if expiry := s.GetNextExpiry(ts); expiry > 0 {
+			client.Zadd("lb.expiry", []byte(s.Name), float64(expiry))
+			return
+		}
+	}
+	client.Zrem("lb.expiry", []byte(s.Name))
+}
+
+
+func GetView(name string) (*View, *View) {
+	view := &View{
+		Name: name,
+		State: STATE_OK,
+	}
+
+	if data, err := client.Get("lb.view." + name); err == nil {
+		json.Unmarshal(data, &view)
+	}
+	var ref = *view
+	return view, &ref
+}
+
+func (s *Service) UpdateViews(channel chan *ViewCmd) {
+	var views, _ = client.Smembers("lb.views.all")
+
+	for _, view := range views {
+		var view_name = string(view)
+		var mbr, _ = client.Sismember("lb.view-contents." + view_name,
+			[]byte(s.Name));
+		if mbr {
+			channel <- &ViewCmd{
+				Action: ACTION_REFRESH_VIEW,
+				View:   view_name,
+			}
+		}
+	}
+}
+
+
+func GetServiceNames() []string {
+	var namesBytes, _ = client.Smembers("lb.services.all")
+
+	var names = make([]string, len(namesBytes))
+	for idx, elem := range namesBytes {
+		names[idx] = string(elem)
+	}
+	return names
+}
+
+
+func GetExpired(ts int64) []*Service {
+	names, err := client.Zrangebyscore("lb.expiry", 0, float64(ts))
+	if err != nil {
+		log.Error("Failed to get expired services: %s", err)
+		return make([]*Service, 0)
+	}
+
+	var services = make([]*Service, len(names))
+	for idx, elem := range names {
+		var s, _ = GetOrCreate(string(elem))
+		services[idx] = s
+	}
+	return services
+}
+
+func GetServices() []*Service {
+	var names, _ = client.Smembers("lb.services.all")
+	var services = make([]*Service, len(names))
+	for idx, elem := range names {
+		var s, _ = GetOrCreate(string(elem))
+		services[idx] = s
+	}
+	return services
+}
+
+func CreateView(view_name string, re *regexp.Regexp, channel chan *ViewCmd) {
+	var key = "lb.view-contents." + view_name
+	client.Del(key)
+	var names, _ = client.Smembers("lb.services.all")
+	for _, name := range names {
+		if re.Match(name) {
+			client.Sadd(key, name)
+		}
+	}
+
+	client.Sadd("lb.views.all", []byte(view_name))
+	log.Info("VIEW '%s' created or updated.", view_name)
+	channel <- &ViewCmd{
+		Action: ACTION_REFRESH_VIEW,
+		View:   view_name,
+	}
+
+}
