9958ad4d640f70d648824b00c8f19ea7a49c4f63
Victor Boivie <victor@boivie.com>
2014-08-18 16:41:17 +0200

    dashboard: Human readable durations

--- data/js/app.js
+++ data/js/app.js
@@ -27,3 +27,38 @@ lovebeatApp.config(['$routeProvider',
         redirectTo: '/views'
       });
   }]);
+
+lovebeatApp.filter('delta_ago', function() {
+  return function(seconds) {
+    return moment.duration(-1 * seconds * 1000).humanize(true);
+  }
+});
+
+lovebeatApp.filter('delta', function() {
+    return function(seconds) {
+	if (seconds < 0)
+	    return "not set";
+	var output = []
+	var numyears = Math.floor(seconds / 31536000);
+	if (numyears > 0) {
+	    output.push(numyears + " yrs")
+	}
+	var numdays = Math.floor((seconds % 31536000) / 86400);
+	if (numdays > 0) {
+	    output.push(numdays + " days")
+	}
+	var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+	if (numhours > 0) {
+	    output.push(numhours + " hrs")
+	}
+	var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+	if (numminutes > 0) {
+	    output.push(numminutes + " min")
+	}
+	var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+	if (numseconds > 0) {
+	    output.push(numseconds + " sec")
+	}
+	return output.join(", ")
+  }
+});
