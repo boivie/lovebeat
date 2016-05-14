3d9d7747325176f89a7de983fcd6557be601bfcc
Victor Boivie <victor@boivie.com>
2016-05-14 23:22:03 +0200

    httpapi: Support for setting timeout without beat

--- api/httpapi.go
+++ api/httpapi.go
@@ -34,7 +34,7 @@ func ServiceHandler(c http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	name := params["name"]
 
-	update := &service.Update{Ts: now(), Service: name, Beat: &service.Beat{}}
+	update := &service.Update{Ts: now(), Service: name}
 
 	// Timeout as query parameter
 	if val, ok := r.URL.Query()["timeout"]; ok {
@@ -45,6 +45,7 @@ func ServiceHandler(c http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 		var t struct {
 			Timeout *int64 `json:"timeout"`
+			Beat    *bool  `json:"beat"`
 		}
 		err := decoder.Decode(&t)
 		if err == nil && t.Timeout != nil {
@@ -53,6 +54,9 @@ func ServiceHandler(c http.ResponseWriter, r *http.Request) {
 			} else {
 				update.SetTimeout = &service.SetTimeout{Timeout: *t.Timeout}
 			}
+		}
+		if err == nil && (t.Beat == nil || *t.Beat == true) {
+			update.Beat = &service.Beat{}
 		}
 	} else {
 		var err = r.ParseForm()
@@ -64,6 +68,8 @@ func ServiceHandler(c http.ResponseWriter, r *http.Request) {
 		if r.FormValue("timeout") != "" {
 			update.SetTimeout = &service.SetTimeout{Timeout: parseTimeout(r.FormValue("timeout"))}
 		}
+
+		update.Beat = &service.Beat{}
 	}
 
 	client.Update(update)
