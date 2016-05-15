5ae89ab9b53ce0c2ed8a8dfce84e428db755f608
Victor Boivie <victor@boivie.com>
2016-05-15 22:13:56 +0200

    dashboard: Manually handle our two assets
    As https://github.com/elazarl/go-bindata-assetfs/pull/26
    isn't fixed yet.

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -1,7 +1,6 @@
 package dashboard
 
 import (
-	assetfs "github.com/elazarl/go-bindata-assetfs"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,9 +13,16 @@ func DashboardHandler(c http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func BundleHandler(c http.ResponseWriter, req *http.Request) {
+	asset, err := bundleJs()
+	if err == nil {
+		c.Header().Set("Content-Type", "text/html")
+		c.Write(asset.bytes)
+	}
+}
+
 func Register(rtr *mux.Router) {
 	rtr.HandleFunc("/", DashboardHandler).Methods("GET")
 	rtr.HandleFunc("/views/{name}", DashboardHandler).Methods("GET")
-	rtr.PathPrefix("/").Handler(http.FileServer(
-		&assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: "/"}))
+	rtr.HandleFunc("/bundle.js", BundleHandler).Methods("GET")
 }
