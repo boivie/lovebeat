2ddcce2940885b36dd5f9b2b318e7ed79c122b3e
Victor Boivie <victor@boivie.com>
2016-06-17 16:12:26 +0200

    Optimize javascript file size in production

--- dashboard/webpack.config.js
+++ dashboard/webpack.config.js
@@ -27,5 +27,16 @@ module.exports = {
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
-  }
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
 };
