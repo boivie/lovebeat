7e48c0311f7d1c8a20e467fc636b3dfb2a1b0396
Victor Boivie <victor@boivie.com>
2016-11-30 22:00:18 +0100

    dashboard: Simplified introduction text

--- dashboard/js/components/home.jsx
+++ dashboard/js/components/home.jsx
@@ -7,15 +7,24 @@ const Home = React.createClass({
       <div id="home">
         <h1>Welcome to Lovebeat</h1>
         <p>
-          For a flull-depth introduction on how to get started, head to <a target="_blank" href="http://lovebeat.readthedocs.io/en/latest/">our documentation</a>.
+          Have you ever had a nightly backup job fail, and it took you weeks 
+          until you noticed it? Say hi to lovebeat, a zero-configuration 
+          heartbeat monitor.
         </p>
-        <p>Try any of these, and then go to the <Link to="/services">services page</Link> to see your heartbeat.</p>
-        <h2>curl</h2>
-        <pre>curl -d timeout=60 http://localhost:8080/api/services/example.service</pre>
-        <h2>python</h2>
-        <pre>import requests<br/>
-        requests.post("http://localhost:8080/api/services/example.service", data=dict(timeout=60))</pre>
-        <p>As long as you keep sending these commands at least once per minute, it will stay green. But when you wait more than one minute, it will turn into a red <b>ERROR</b> state.</p>
+        <p>
+          We have a great chapter in our documentation 
+          on <a target="_blank" href="http://lovebeat.readthedocs.io/en/latest/getting_started.html">getting started</a> with 
+          Lovebeat.
+        </p>
+        <p>
+          If you're impatient, you can try the following and see the results in the <Link to="/services">services page</Link>:
+        </p>
+        <pre>curl -d timeout=10 http://localhost:8080/api/services/example.service</pre>
+        <p>
+          As long as you keep running that command (within the 10 second timeout), the service will stay green. 
+          But if you stop, the service will become red and say <b>ERROR</b>. When that happens, you can configure
+          lovebeat to run a shell script, send an e-mail or post to Slack.
+        </p>
       </div>
     );
   }
